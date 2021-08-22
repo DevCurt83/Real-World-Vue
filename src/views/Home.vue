a3d09317209437424ceb739fa3d490e0f0833db6
Curtis <curt_king@coolsite.net>
2021-08-22 11:14:49 +0100

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />   <!-- msg prop is being fed in here -->
   </div>
 </template>
 
